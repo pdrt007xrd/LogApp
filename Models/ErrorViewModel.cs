07d5e5c7915704a64d0ba079d102608b7afef531
Pedro <pedrito007xrd@gmail.com>
2023-08-23 19:02:44 -0400

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projecto_Oficina.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
